1d2d5c1d6201b389608c764a82bd1727678d824d
AHMET UCAR <213214006@stu.adu.edu.tr>
2022-09-13 03:58:01 +0300

    finished

---
+++ index.js
@@ -0,0 +1,16 @@
+function activeSidebar () {
+    document.getElementById('sidebarLeftMobile').classList.toggle('active');
+}
+
+function closeSidebar () {
+    document.getElementById('sidebarLeftMobile').classList.toggle('close');
+}
+
+function activeMessages () {
+    document.getElementById('onMessageBox').classList.toggle('on');
+}
+
+
+function offMessageBox () {
+    document.getElementById('onMessageBox').classList.toggle('off');
+}
